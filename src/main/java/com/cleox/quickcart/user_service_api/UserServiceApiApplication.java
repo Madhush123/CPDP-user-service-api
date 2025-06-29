8a1796c038ec502fb8286e1b533953f073e87357
Madhushan <madhushan999@gmail.com>
2025-06-29 19:06:25 +0530

    1

--- src/main/java/com/cleox/quickcart/user_service_api/UserServiceApiApplication.java
+++ src/main/java/com/cleox/quickcart/user_service_api/UserServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.cleox.quickcart.user_service_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
 public class UserServiceApiApplication {
 
 	public static void main(String[] args) {
